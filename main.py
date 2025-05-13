227b0c688ee19be122f38e74280dd7be2bac2882
yjun0915 <kyjun09150916@gmail.com>
2025-05-13 17:51:57 +0900

    non

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from pptx import Presentation
 
 prs = Presentation()
+
+layout = prs.slide_layouts[0]
+
+slide = prs.slides.add_slide(layout)
+
+title = slide.shapes[0]
+
+subtitle = slide.shapes[1]
+
+title.text = "Hello, World!"
+
+subtitle.text = "python-pptx"
+
+prs.save("./ppt/test.pptx")
